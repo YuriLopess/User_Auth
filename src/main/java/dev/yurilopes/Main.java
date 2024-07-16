a9069de6a5bc2f375c01d671c319d3082ad68007
YuriLopess <costalopesyuri@gmail.com>
2024-07-16 18:58:38 -0300

    Adding the email verification methods.

--- src/main/java/dev/yurilopes/Main.java
+++ src/main/java/dev/yurilopes/Main.java
@@ -1,7 +1,52 @@
 package dev.yurilopes;
 
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+        Util util = new Util();
+        Map<String, String> users = new HashMap<>();
+
+        byte option;
+        boolean status;
+
+        try {
+
+            do {
+                option = util.showMenu();
+
+                switch (option) {
+
+                    default:
+                        System.out.println("nenhum");
+
+                    case 1 :
+
+                        String email = util.getValidEmail();
+                        String password = util.getValidPassword();
+
+                        users.put(email, password);
+                        System.out.println("Email cadastrado com sucesso!");
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+                        System.out.println("Test - Saindo..");
+                        break;
+                }
+
+            } while (option != 3);
+
+        } catch (InputMismatchException e) {
+            System.out.println("erro");
+        }
     }
 }
--- src/main/java/dev/yurilopes/Util.java
+++ src/main/java/dev/yurilopes/Util.java
@@ -1,5 +1,68 @@
 package dev.yurilopes;
 
+import java.awt.desktop.ScreenSleepEvent;
+import java.util.Scanner;
+
 public class Util {
-    
+
+    Scanner scanner = new Scanner(System.in);
+
+    byte showMenu() {
+        System.out.println("""
+                Escolha sua oção:
+                
+                1 - Criar conta
+                2 - Fazer login
+                3 - Sair
+                
+                Qual a sua opção:""");
+
+        return scanner.nextByte();
+    }
+
+    String askForEmail() {
+        System.out.println("Digite o email:");
+        return scanner.next();
+    }
+
+    boolean validationEmail(String enteredEmail) {
+        return (enteredEmail.contains("@") && enteredEmail.contains(".com"));
+    }
+
+    String getValidEmail() {
+        String email = askForEmail();
+        boolean status = validationEmail(email);
+
+        while (!status) {
+            System.out.println("Tente de novo, email não valido.");
+            email = askForEmail();
+            status = validationEmail(email);
+        }
+        return email;
+    }
+
+    String askForPassword() {
+        System.out.println("Digite a senha:");
+        return scanner.next();
+    }
+
+    boolean validationPassword(String enteredPassword) {
+        String regex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{7,}$";
+        return enteredPassword.matches(regex);
+    }
+
+    String getValidPassword() {
+        String password = askForPassword();
+        boolean status = validationPassword(password);
+
+        while (!status) {
+            System.out.println("Tente de novo, senha não valida");
+            password = askForPassword();
+            status = validationPassword(password);
+        }
+        return password;
+    }
+
+
+
 }

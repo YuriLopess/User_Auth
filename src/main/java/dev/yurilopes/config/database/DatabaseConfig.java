a131299b334ab5b8d8ad8799c3a86aa0299c2d4c
YuriLopess <costalopesyuri@gmail.com>
2024-07-17 17:12:56 -0300

    Creating connection to the MySQL database.

---
+++ src/main/java/dev/yurilopes/config/database/DatabaseConfig.java
@@ -0,0 +1,41 @@
+package dev.yurilopes.config.database;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConfig {
+
+    private static Properties loadProperties() {
+        Properties props = new Properties();
+        try (InputStream input = DatabaseConfig.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                throw new IOException("Properties file not found");
+            }
+            props.load(input);
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar propriedades: " + e.getMessage());
+        }
+        return props;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Properties props = loadProperties();
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = getConnection()) {
+            System.out.println("Conexão estabelecida com sucesso...");
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+        }
+    }
+}
